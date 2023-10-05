1d010ef6a5fc455f0e4442945c0fd7cedd05483e
RPG-Alex <45256796+RPG-Alex@users.noreply.github.com>
2023-10-05 18:56:35 +0800

    Started work on iterator for posts

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -6,6 +6,7 @@ const COPYRIGHT_YEAR: u16 = 2023;
 
 //Structure for a blog post
 struct Post {
+	id: usize,
 	title: String,
 	date: String,
 	body: String,
@@ -15,18 +16,32 @@ struct Post {
 #[function_component]
 fn App() -> Html {
 	let home = "index.html";
-	//test blog post. 
-	let example_post = Post{
-		title: String::from("This is generated from a struct!"),
-		date: chrono::Local::now().year().to_string(),
-		body: String::from("Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, voluptas sed sapiente hic dolores qui, beatae eaque at repellendus illum aliquid animi laudantium labore dolorum unde fuga vero, sit perspiciatis."),
-	};
+	//test blog posts list. 
+	let example_posts = vec![
+		Post{
+			id: 1,
+			title: String::from("This is generated from a struct!"),
+			date: chrono::Local::now().year().to_string(),
+			body: String::from("Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, voluptas sed sapiente hic dolores qui, beatae eaque at repellendus illum aliquid animi laudantium labore dolorum unde fuga vero, sit perspiciatis."),
+		},
+		Post{
+			id: 2,
+			title: String::from("This is another generated from a struct!"),
+			date: chrono::Local::now().year().to_string(),
+			body: String::from("This post is only visible of you click! This post is only visible of you click! This post is only visible of you click! This post is only visible of you click! This post is only visible of you click! This post is only visible of you click! This post is only visible of you click! This post is only visible of you click!"),
+		}
+	];
+	//Iterator for posts and potential blog post template
+	let example_posts = example_posts.iter().map(|post| html!{
+		<p>
+		<a key={post.id} href={post.id.to_string()}>{format!("{}", post.title)}</a>
+		</p>
+	}).collect::<Html>();
 	html!{
 		<>
 		<header><a href={home}>{"Home"}</a></header>
-		<h1>{example_post.title}</h1>
-		<p>{example_post.body}</p>
-		<footer>{"Copyright: "}<b>{COPYRIGHT_YEAR}{"-"}{example_post.date}</b></footer>
+		{example_posts}
+		<footer>{"Copyright: "}<b>{COPYRIGHT_YEAR}</b></footer>
 		</>
 	}
 }
