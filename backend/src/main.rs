711f11047b043f41325cdf26ee0d2ac4a5169614
RPG-Alex <45256796+RPG-Alex@users.noreply.github.com>
2023-10-07 21:27:05 +0800

    From yew example can now interact with posts!

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,11 @@
+/* TODO:
+- CREATE REST API for frontend to access
+    - Create database control structure
+    - Add functions for reading from database first
+    - Implement HTTP GET request
+
+*/
+
 fn main() {
     println!("Hello, world!");
 }
