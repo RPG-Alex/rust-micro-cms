a794fdcb4206b1f872b642db6fc79623eb70f3ca
RPG-Alex <mralexblood@outlook.com>
2023-10-29 17:40:16 +0800

    working on the blogpost class!

--- frontend/src/lib.rs
+++ frontend/src/lib.rs
@@ -14,13 +14,13 @@ pub fn ListAllBlogPost() -> impl IntoView {
 		Post{
 			id: 1,
 			title: String::from("This is generated from a struct!"),
-			date: chrono::Local::now().year().to_string(),
+			date: chrono::Local::now().date_naive().to_string(),
 			body: String::from("Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, voluptas sed sapiente hic dolores qui, beatae eaque at repellendus illum aliquid animi laudantium labore dolorum unde fuga vero, sit perspiciatis."),
 		},
 		Post{
 			id: 2,
 			title: String::from("This is another generated from a struct!"),
-			date: chrono::Local::now().year().to_string(),
+			date: chrono::Local::now().date_naive().to_string(),
 			body: String::from("This post is only visible of you click! This post is only visible of you click! This post is only visible of you click! This post is only visible of you click! This post is only visible of you click! This post is only visible of you click! This post is only visible of you click! This post is only visible of you click!"),
 		}
 	];
@@ -29,16 +29,23 @@ pub fn ListAllBlogPost() -> impl IntoView {
 		<h1>"To do: show all posts!"</h1>
 		<ul>
 			{posts.into_iter()
-				.map(|n| view! {
+				.map(|n| 
+					view! {
 					<li>
-					<b>"TITLE: "</b> 
-					{n.title}
-					"DATE: "
-					{n.date}
-					{n.body}
+						{&n.title}
+						<ShowSinglePostInfo post=n />
 					</li>
 				}).collect_view()
 			}
 		</ul>
     }
+}
+
+#[component]
+fn ShowSinglePostInfo(post: Post) -> impl IntoView {
+	
+	view! {
+		<h2>{post.date}</h2>
+		<p>{post.body}</p>
+	}
 }
