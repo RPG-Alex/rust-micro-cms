f1a3f568660f0b1fd646817ab7a9bf0bfb105787
RPG-Alex <mralexblood@outlook.com>
2024-01-17 21:15:03 +0800

    Successfully fixed POST bug. Able to post to DB

--- src/api.rs
+++ src/api.rs
@@ -1,16 +1,16 @@
 use axum::{
-    Form,
+    http::StatusCode,
     Json,
     extract::Extension, Error,
     response::IntoResponse,
+    
 };
 use chrono::Utc;
 use r2d2::Pool;
 use r2d2_sqlite::SqliteConnectionManager;
-use serde::Deserialize;
+use serde::{Serialize, Deserialize};
 use serde_json::json;
 use std::sync::Arc;
-use std::convert::Infallible;
 use crate::db::{self};
 
 //API Endpoint for all posts
@@ -30,25 +30,44 @@ pub async fn fetch_all_posts_as_json(db_pool: Extension<Arc<Pool<SqliteConnectio
 
 // Structure used for submitting new posts
 #[derive(Deserialize)]
-struct NewPost {
+pub struct NewPost {
     title: String,
     author_id: usize,
     body: String,
 }
 
+// Response type for successful post creation (thank you documentation!)
+#[derive(Serialize)]
+pub struct PostResponse {
+    status: String,
+    message: String,
+}
+
 // Add a new post
-pub async fn add_post(form: Form<NewPost>, db_pool: Extension<Arc<Pool<SqliteConnectionManager>>>) -> Result<Json<serde_json::Value>, Error> {
+#[axum::debug_handler]
+pub async fn add_post(
+    db_pool: Extension<Arc<Pool<SqliteConnectionManager>>>,
+    Json(new_post): Json<NewPost>, 
+) -> impl IntoResponse {
     let pool = db_pool.0;
     let conn = pool.get().expect("Failed to get a connection from the pool");
 
-    // Extracting data from the form
-    let new_post = form.0;
     let date = Utc::now();
-    // Inserting the new post into the database
-    //      This needs to be corrected. 
     match db::create_post(&conn, &new_post.title, &date.to_string(), &new_post.body, new_post.author_id) {
-        Ok(_) => Ok(Json(json!({ "status": "success", "message": "Post added successfully" }))),
-        Err(e) => Ok(Json(json!({ "status": "error", "message": e.to_string() })))
+        Ok(_) => (
+            StatusCode::OK,
+            Json(PostResponse {
+                status: "success".to_string(),
+                message: "Post added successfully".to_string(),
+            }),
+        ),
+        Err(e) => (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            Json(PostResponse {
+                status: "error".to_string(),
+                message: e.to_string(),
+            }),
+        ),
     }
 }
 
