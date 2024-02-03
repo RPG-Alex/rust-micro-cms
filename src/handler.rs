04ea30f48c89029e57247a5aaccb27636f0c32ce
RPG-Alex <mralexblood@outlook.com>
2024-02-03 10:19:32 +0800

    starting to implement a handler

---
+++ src/handler.rs
@@ -0,0 +1,20 @@
+/*
+TODO List for Handler.rs
+
+1. Setup and Initialization
+
+2. Post Operations
+
+3. Author Operations
+
+4. Rendering and Forms
+
+5. Authentication and Authorization (If applicable)
+
+6. Error Handling
+
+7. API Versioning and Documentation
+
+8. Test Handlers
+
+*/
