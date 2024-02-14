765e2b905999b7776e09f7c9da8cb010e9234a65
RPG-Alex <mralexblood@outlook.com>
2024-02-14 20:56:17 +0800

    Added Skeleton for more OOP style modularity. Will refactor to this before moving forward

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,6 @@ use r2d2::Pool;
 
 #[tokio::main]
 async fn main() {
-    // Set path to database and establish a connection pool
     let db_path = Path::new("posts.db");
     
     let manager = SqliteConnectionManager::file(db_path);
@@ -22,11 +21,10 @@ async fn main() {
     initialize_database_tables(&pool).await.expect("Failed to initialize database tables");
 
 
-    // Set up the Axum application with routes
-    let app = Router::new()
+        let app = Router::new()
         // App root and APIs
-        .route("/posts", get(api::fetch_all_posts_as_json)) // Default route serves JSON version of all posts
-        .route("/posts/new", post(api::add_post)) // add new posts
+        .route("/posts", get(api::fetch_all_posts_as_json)) 
+        .route("/posts/new", post(api::add_post)) 
         .route("/posts/delete", delete(api::delete_post)) 
         .route("/posts/update", post(api::update_post))
 
@@ -36,17 +34,15 @@ async fn main() {
         .route("/authors/:id", get(api::fetch_author_info_as_json))
         
         //All rendering goes here
-        .route("/post", get(render::render_all_posts)) // "/posts" route serves HTML version of all posts
-        .route("/post/:id", get(render::render_single_post)) // "/post/:id" route serves individual post in HTML
-        .route("/post/new", get(render::render_add_post_form)) // create a new post
+        .route("/post", get(render::render_all_posts)) 
+        .route("/post/:id", get(render::render_single_post)) 
+        .route("/post/new", get(render::render_add_post_form))
         .route("/post/add_post", post(api::add_post))
         .layer(axum::extract::Extension(Arc::new(pool)));
 
-    // Define the server address
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     println!("Server running at http://{}", addr);
 
-    // Run the Axum server
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
