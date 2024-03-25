3a8c90be25087491a206ef389a31f9c033f23166
RPG-Alex <mralexblood@outlook.com>
2024-03-25 21:21:43 +0800

    Added User Model and cleaned up directories

---
+++ rust-micro-cms/src/models/authors.rs
@@ -0,0 +1,29 @@
+use serde::{Serialize, Deserialize};
+
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct NewAuthor {
+    pub first_name: String,
+    pub last_name: String,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct Author {
+    pub id: i64,
+    pub first_name: String,
+    pub last_name: String,
+    pub deleted: bool,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct UpdateAuthor {
+    pub id: i64,
+    pub first_name: String,
+    pub last_name: String,
+    pub deleted: bool,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct Authors {
+    pub authors: Vec<Author>,
+}
