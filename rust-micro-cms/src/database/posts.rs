69ab37291259da5b18ea55a98dd91f79b35bf435
Alex Blood <mralexblood@outlook.com>
2024-05-07 11:18:11 +0800

    update post has been rewritten

--- rust-micro-cms/src/database/posts.rs
+++ rust-micro-cms/src/database/posts.rs
@@ -78,41 +78,41 @@ pub async fn fetch_post_by_id(conn: &Connection, post_id: i32) -> Result<Option<
     Ok(Some(stmt))
 }
 
-pub async fn fetch_all_posts_for_author(conn: &Connection, author_id: i32) -> Result<Posts> {
-    let posts = sqlx::query_as!(
-        Post,
-        "SELECT * FROM posts WHERE author_id = $1 AND archived = FALSE",
-        author_id
-    )
-    .fetch_all(pool)
-    .await?;
+// pub async fn fetch_all_posts_for_author(conn: &Connection, author_id: i32) -> Result<Posts> {
+//     let posts = sqlx::query_as!(
+//         Post,
+//         "SELECT * FROM posts WHERE author_id = $1 AND archived = FALSE",
+//         author_id
+//     )
+//     .fetch_all(pool)
+//     .await?;
 
-    Ok(Posts { posts })
-}
+//     Ok(Posts { posts })
+// }
 
 pub async fn update_post(conn: &Connection, post: &UpdatePost) -> Result<Post> {
-    let updated_post = sqlx::query!(
-        "UPDATE posts SET title = $1, date = $2, body = $3, archived = $4, draft = $5, author_id = $6 WHERE id = $7 RETURNING *",
-        post.title,
-        post.date,
-        post.body,
-        post.archived,
-        post.draft,
-        post.author_id,
-        post.id
-    )
-    .fetch_one(pool)
-    .await?;
+    let sql = "UPDATE posts SET title = $1, date = $2, body = $3, archived = $4, draft = $5, author_id = $6 WHERE id = $7 RETURNING *";
+    let stmt = conn.query_row(sql, [
+        post.title.to_owned(),
+        post.date.to_owned(),
+        post.body.to_owned(),
+        post.archived.to_string(),
+        post.draft.to_string(),
+        post.author_id.to_string(),
+        post.id.to_string()
+    ], |row| {
+        Ok(Post {
+            id: row.get(0)?,
+            title: row.get(1)?,
+            date: row.get(2)?,
+            body: row.get(3)?,
+            archived: row.get(4)?,
+            draft: row.get(5)?,
+            author_id: row.get(6)?,
+        })
+    })?;
 
-    Ok(Post {
-        id: updated_post.id.expect("Could not find id"),
-        title: updated_post.title,
-        date: updated_post.date,
-        body: updated_post.body,
-        archived: updated_post.archived,
-        draft: updated_post.draft,
-        author_id: updated_post.author_id,
-    })
+    Ok(stmt)
 }
 
 pub async fn delete_post(conn: &Connection, post_id: i32) -> Result<()> {
