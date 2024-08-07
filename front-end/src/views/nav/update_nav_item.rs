4f244e9e9f0db5a95476d28ab8e55b7b715952b3
Alex <mralexblood@outlook.com>
2024-08-07 08:46:38 -0600

    mapped props for nav update

--- front-end/src/views/nav/update_nav_item.rs
+++ front-end/src/views/nav/update_nav_item.rs
@@ -14,6 +14,10 @@ pub struct UpdateNavItemProps {
 
 #[function_component(UpdateNavItemForm)]
 pub fn update_nav_item_form(props: &UpdateNavItemProps) -> Html {
+    let post_id = use_state(|| props.update_nav.id.clone());
+    let item_type = use_state(|| props.update_nav.item_type.clone());
+    let content = use_state(|| props.update_nav.content.clone());
+    let url = use_state(|| props.update_nav.url.clone());
     html! {
         <div>
             {"Boiler plate for updating nav message"}
