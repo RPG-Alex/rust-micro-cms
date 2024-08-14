4cec0581ec4322fcd39a11e5eb5d5b38e1fe989b
RPG-Alex <mralexblood@outlook.com>
2024-08-14 21:29:23 +0800

    site info table fn created.

--- rust-micro-cms/src/database/site_info.rs
+++ rust-micro-cms/src/database/site_info.rs
@@ -9,3 +9,21 @@ pub struct SiteInfo {
     pub author_name: String,
     pub description: Option<String>,
 }
+
+pub async fn create_site_info_table(pool: &Pool<SqliteConnectionManager>) -> Result<usize> {
+    let conn = pool
+    .get()
+    .map_err(|_| rusqlite::Error::ExecuteReturnedResults)?;
+    let sql = "CREATE TABLE IF NOT EXISTS posts (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        site_name TEXT NOT NULL,
+        author_name TEXT NOT NULL,
+        description TEXT
+    )";
+    conn.execute(sql, ())
+}
+/*
+TODO:
+    Implement the CRUD for the site info
+    
+*/
