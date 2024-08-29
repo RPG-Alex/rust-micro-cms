3e5b9b0f9b0c25e5574a741aa3f80040c9e42965
Alex <mralexblood@outlook.com>
2024-08-29 21:34:02 +0800

    working on fetch site info

--- rust-micro-cms/src/database/site_info.rs
+++ rust-micro-cms/src/database/site_info.rs
@@ -1,12 +1,14 @@
 use r2d2::Pool;
 use r2d2_sqlite::SqliteConnectionManager;
 use rusqlite::{params, Result};
+use crate::models::{SiteInfo, SiteName, AuthorName, Description};
+
 
 pub async fn create_site_info_table(pool: &Pool<SqliteConnectionManager>) -> Result<usize> {
     let conn = pool
     .get()
     .map_err(|_| rusqlite::Error::ExecuteReturnedResults)?;
-    let sql = "CREATE TABLE IF NOT EXISTS posts (
+    let sql = "CREATE TABLE IF NOT EXISTS site_info (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         site_name TEXT NOT NULL,
         author_name TEXT NOT NULL,
@@ -14,8 +16,22 @@ pub async fn create_site_info_table(pool: &Pool<SqliteConnectionManager>) -> Res
     )";
     conn.execute(sql, ())
 }
-/*
-TODO:
-    Implement the CRUD for the site info
-    
-*/
+
+pub async fn fetch_site_info(pool: &Pool<SqliteConnectionManager>) -> Result<SiteInfo> {
+    let conn = pool
+        .get()
+        .map_err(|_| rusqlite::Error::ExecuteReturnedResults)?;
+    let sql = "SELECT * from site_info";
+    let mut stmt = conn.prepare(sql)?;
+    //Modify this later so that it only ever returns one row. There should never be more than one row. Reinforce that here.
+    let info_iter = stmt.query_map([], |row| {
+        Ok(SiteInfo {
+            id: row.get(0)?,
+            name: row.get(1)?,
+            author_name: row.get(2)?,
+            description: row.get(3)?,
+        })
+    })?;
+
+    // need to extract the first row. 
+}
